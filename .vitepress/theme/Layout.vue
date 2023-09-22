7a340a1a45a29df3b2bb8b766537cc671c5a35ef
gernest <geofreyernest@live.com>
2023-09-22 13:45:00 +0300

    fix paths

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -11,15 +11,15 @@ const { page, frontmatter } = useData()
   <div class="antialiased dark:bg-slate-900">
     <div class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
       <nav class="flex justify-between items-center py-10 font-bold">
-        <a class="text-xl" href="/" aria-label="Vince Analytics">
-          <img class="inline-block mr-2" style="width: 36px; height: 31px" alt="logo" src="/logo.svg" />
+        <a class="text-xl" href="/blog/" aria-label="Vince Analytics">
+          <img class="inline-block mr-2" style="width: 36px; height: 31px" alt="logo" src="/blog/logo.svg" />
           <span v-if="!frontmatter.index" class="hidden md:inline dark:text-white">Vince Analytics</span>
         </a>
         <div class="text-sm text-gray-500 dark:text-white leading-5">
           <a class="hover:text-gray-700 dark:hover:text-gray-200" href="https://github.com/vinceanalytics/vince"
             target="_blank" rel="noopener"><span class="hidden sm:inline">GitHub </span>Source</a>
           <span class="mr-2 ml-2">·</span>
-          <a class="hover:text-gray-700 dark:hover:text-gray-200" href="/feed.rss">RSS<span class="hidden sm:inline">
+          <a class="hover:text-gray-700 dark:hover:text-gray-200" href="/blog/feed.rss">RSS<span class="hidden sm:inline">
               Feed</span></a>
           <span class="mr-2 ml-2">·</span>
           <a class="hover:text-gray-700 dark:hover:text-gray-200" href="https://vinceanalytics.com" target="_blank"
